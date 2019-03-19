8837aff69b7412e7345d0dfd85d9b947e11c0e75
Arda Akdemir <ardaakdemir1@gmail.com>
2019-03-19 22:09:12 +0900

    aa

--- Sentence/fill_in_blanks.py
+++ Sentence/fill_in_blanks.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import sys
 import re
-
+import json
+import os
 filename = sys.argv[1]
 
+print(filename)
+#d = json.load(open(filename,"rb"))
 df = pd.read_json(filename, orient="records", lines=True)
 df["offset"] = ""
 df["text"] = ""
-
+listed_news = os.listdir("../collector/tmp/texts")
+print(len(listed_news))
 # We basically remove all the non-alphanumerical characters from our string and substring, then do the match. Later we map the resulting offsets to the original string we were searching.
 def map_to_withW(i, j, text):
     tmp = re.findall(r"\W", text[i:j])
@@ -41,62 +45,71 @@ def fill_blank(row, els):
     row.offset = start + "," + end
     row.sentence = row.text[int(start):int(end)]
     return row
-
+count = 0
+total = 0
 for url in df.url.unique().tolist():
+    total +=1
     els = df[df.url == url]
     text_file = re.sub(r":\/\/", r"___", url)
     text_file = re.sub(r"\/", r"_", text_file)
-    print(text_file)
-    with open("tmp/texts/" + text_file, "r", encoding="utf-8") as f:
-        text = f.read()
+    #print(text_file)
+    text_file_full = "../collector/tmp/texts/" + text_file
+    if text_file not in listed_news:
+        continue
+    else:
+        with open(text_file_full, "r", encoding="utf-8") as f:
+            text = f.read()
 
-    df.loc[df.url == url, "text"] = text
-    print(text)
-    nonW_list = []
-    for match in re.finditer(r"\W", text):
-        nonW_list.append(match.start())
+        df.loc[df.url == url, "text"] = text
+        #print(text)
+        nonW_list = []
+        for match in re.finditer(r"\W", text):
+            nonW_list.append(match.start())
 
-    first_sent = els[els.sent_num == 1].sentence.iloc[0]
-    sep = int(len(first_sent) / 2)
-    # print(first_sent)
-    # print("--")
-    # print(first_sent[sep:])
-    
-    #start = re.search(re.escape(first_sent[sep:]), text)
-    nonW_text = re.sub(r"\W", r"", text)
-    start = re.search(re.sub(r"\W", r"", first_sent[sep:]), nonW_text)
+        first_sent = els[els.sent_num == 1].sentence.iloc[0]
+        sep = int(len(first_sent) / 2)
+        # print(first_sent)
+        # print("--")
+        # print(first_sent[sep:])
 
-    eom, som = get_eom(start, text)
+        #start = re.search(re.escape(first_sent[sep:]), text)
+        nonW_text = re.sub(r"\W", r"", text)
+        start = re.search(re.sub(r"\W", r"", first_sent[sep:]), nonW_text)
+        print(count)
+        try:
+            eom, som = get_eom(start, text)
+            # df.loc[(df.url == url) & (df.sent_num == 1), "offset"] = (max(0, som-sep-1), eom)
+            df.loc[(df.url == url) & (df.sent_num == 1), "offset"] = str(max(0, som-sep-1)) + "," + str(eom)
+            # print(text[som:eom+1])
+            # print("--")
+            # print(text[max(0, som-sep-1):eom+1])
+            # print("-------------------------------")
 
-    # df.loc[(df.url == url) & (df.sent_num == 1), "offset"] = (max(0, som-sep-1), eom)
-    df.loc[(df.url == url) & (df.sent_num == 1), "offset"] = str(max(0, som-sep-1)) + "," + str(eom)
-    # print(text[som:eom+1])
-    # print("--")
-    # print(text[max(0, som-sep-1):eom+1])
-    # print("-------------------------------")
+            #end = re.search(re.escape(els[els["last"] == True].sentence.iloc[0]), text)
+            # end = re.search(re.escape(els[els["last"] == True].sentence.iloc[0]), text)
+            # print(start)
+            # print(end.end())
+            # df["text"] = text[start.start()-sep:end.end()-1]
 
-    #end = re.search(re.escape(els[els["last"] == True].sentence.iloc[0]), text)
-    # end = re.search(re.escape(els[els["last"] == True].sentence.iloc[0]), text)
-    # print(start)
-    # print(end.end())
-    # df["text"] = text[start.start()-sep:end.end()-1]
+            sentences = els[(els.sent_num != 1) & (els.sentence != "REDACTED")]
+            for i in range(len(sentences)):
+                sent = sentences.iloc[i]
+                if len(sent.sentence) >= 50: # Arbitrary number. If the sentence is too small, use the whole sentence to match.
+                    sep = int(len(sent.sentence) / 5)
+                    match = re.search(re.sub(r"\W", r"", sent.sentence[:sep]), nonW_text)
+                    som = get_som(match, text)
+                    match = re.search(re.sub(r"\W", r"", sent.sentence[len(sent.sentence)-sep:]), nonW_text)
+                    eom, _ = get_eom(match, text)
+                else:
+                    match = re.search(re.sub(r"\W", r"", sent.sentence), nonW_text)
+                    eom, som = get_eom(match, text)
 
-    sentences = els[(els.sent_num != 1) & (els.sentence != "REDACTED")]
-    for i in range(len(sentences)):
-        sent = sentences.iloc[i]
-        if len(sent.sentence) >= 50: # Arbitrary number. If the sentence is too small, use the whole sentence to match.
-            sep = int(len(sent.sentence) / 5)
-            match = re.search(re.sub(r"\W", r"", sent.sentence[:sep]), nonW_text)
-            som = get_som(match, text)
-            match = re.search(re.sub(r"\W", r"", sent.sentence[len(sent.sentence)-sep:]), nonW_text)
-            eom, _ = get_eom(match, text)
-        else:
-            match = re.search(re.sub(r"\W", r"", sent.sentence), nonW_text)
-            eom, som = get_eom(match, text)
-
-        # sentences.loc[sentences.sentence == sent.sentence, "offset"] = (som, min(eom, len(text)))
-        sentences.loc[sentences.sentence == sent.sentence, "offset"] = str(som) + "," + str(min(eom, len(text)))
-
+                # sentences.loc[sentences.sentence == sent.sentence, "offset"] = (som, min(eom, len(text)))
+                sentences.loc[sentences.sentence == sent.sentence, "offset"] = str(som) + "," + str(min(eom, len(text)))
+        except:
+            count+=1
+    print(count)
+    print(total)
     df.loc[(df.url == url) & (df.sent_num != 1) & (df.sentence != "REDACTED")] = sentences
 
 # df.to_csv("asd.csv", index=False)
--- Sentence/run.sh
+++ Sentence/run.sh
@@ -15,7 +15,7 @@ for data_file in $data_path/*.json
 do
     scrapy crawl sp -a filename="$data_file"
     python3 -W ignore getnews_selenium.py $data_file
-    
+
     for filename in tmp/htmls/http*
     do
 	ofile=$(echo "$(basename $filename)")
@@ -24,21 +24,22 @@ do
 		python3 justext_gettext.py $ofile
 		python3 preprocess_timesofindia.py $ofile
 	    elif [[ $ofile == *"newindianexpress"* ]]; then
-		python goose_gettext.py $ofile
+		python2 goose_gettext.py $ofile
 		python3 preprocess_newindianexpress.py $ofile
 	    elif [[ $ofile == *"indianexpress"* ]]; then
-		python goose_gettext.py $ofile
+		python2 goose_gettext.py $ofile
 		python3 preprocess_indianexpress.py $ofile
 	    elif [[ $ofile == *"thehindu"* ]]; then
-		python boilerpipe_gettext.py $ofile
+		python2 boilerpipe_gettext.py $ofile
 		python3 preprocess_thehindu.py $ofile
 	    elif [[ $ofile == *"scmp"* ]]; then
-		python boilerpipe_gettext.py $ofile
+		python2 boilerpipe_gettext.py $ofile
 		python3 preprocess_scmp.py $ofile
 	    else
 		echo "No idea what source : $ofile"
 	    fi
 	fi
+  echo 'arda'
     done
 
     python3 $data_path/fill_in_blanks.py $data_file
