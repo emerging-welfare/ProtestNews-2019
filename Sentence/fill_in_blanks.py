5033758c87eb52c1bf0330057e9402a74eb0712a
Arda Akdemir <ardaakdemir1@gmail.com>
2019-03-22 21:42:13 +0900

    fix fill in blanks

--- Sentence/fill_in_blanks.py
+++ Sentence/fill_in_blanks.py
@@ -108,10 +108,10 @@ for url in df.url.unique().tolist():
 #print("No doc count : ", nodoc_count)
 #print("No first match count : ", nofirstmatch_count) # If the first sentence cannot be found in downloaded text
 #print("No any match count : ", noanymatch_count) # If any odd numbered sentence other than first cannot be found in downloaded text
-logging.info("Total doc count : %d", %total)
-logging.info("No doc count : %d", %nodoc_count)
-logging.info("No first match count %d: ", %nofirstmatch_count) # If the first sentence cannot be found in downloaded text
-logging.info("No any match count : %d", %noanymatch_count)
+logging.info("Total doc count : %d" %total)
+logging.info("No doc count : %d" %nodoc_count)
+logging.info("No first match count %d: " %nofirstmatch_count) # If the first sentence cannot be found in downloaded text
+logging.info("No any match count : %d" %noanymatch_count)
 
 df = df[df.text.str.strip() != ""]
 
